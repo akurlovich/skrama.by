4e47ce45022d453871f9faad80b44238e77c917a
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-23 13:47:50 +0300

    fix: context data

--- client/src/components/TypeBlock/TypeBlock.tsx
+++ client/src/components/TypeBlock/TypeBlock.tsx
@@ -13,11 +13,11 @@ const TypeBlockInner: FC = () => {
       <div className="card__content">
         <div className="details">
           <h2>Поликабронат<br></br><span>Сотовый</span></h2>
-          <div className="data">
+          {/* <div className="data">
             <h3>342<br></br><span>Posts</span></h3>
             <h3>120k<br></br><span>Folowers</span></h3>
             <h3>285<br></br><span>Folowing</span></h3>
-          </div>
+          </div> */}
           <div className="actionBtn">
             <button>Перейти</button>
           </div>
