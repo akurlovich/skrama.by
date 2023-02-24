adb17a8bd3f979c92b54ada69b53e37ad54a43ec
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2023-02-24 13:42:01 +0300

    Update http.ts

--- client_V2/src/constants/http.ts
+++ client_V2/src/constants/http.ts
@@ -1,2 +1,6 @@
-export const SERVER_URL = 'http://localhost:4000/';
+// export const SERVER_URL = 'http://localhost:4000/';
+export const SERVER_URL = 'https://skrama-server.onrender.com/';
 export const API_URL = SERVER_URL + 'api/';
+
+
+// https://skrama-server.onrender.com
