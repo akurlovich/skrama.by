0940d428de2748e0440a0510f0855106621d20b9
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2023-02-24 14:11:45 +0300

    delete: unused components

--- client_V2/src/components/AddProduct/AddProductBrand/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react'
-
-const AddProductBrandInner: FC = () => {
-  return (
-    <div>AddProductType</div>
-  )
-}
-
-export const AddProductBrand = React.memo(AddProductBrandInner)
